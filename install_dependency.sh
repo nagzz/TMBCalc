535353a2f94e5cead4fc151874f3b461f5f5461c
gretep <57007795+gretep@users.noreply.github.com>
2021-10-15 13:16:13 +0200

    Modification

--- install_dependency.sh
+++ install_dependency.sh
@@ -9,6 +9,7 @@
 #cd java-se-8u41-ri/bin/java
 #Se java 8 installato con sudo mettere in path java solo java
 #tar xzf annovar.latest.tar.gz
+#if per hg19/hg38/both
 
 #!/bin/bash
 
@@ -26,18 +27,24 @@ if [ $perm == "yes" ];
 then #Controlla se ho scritto l'if giusto
   sudo apt install -y samtools
   sudo apt install -y bowtie2
+  sudo apt install -y cutadapt
+  sudo apt install -y fastqc
   sudo apt install -y python3-pip
   sudo apt install -y unzip
   sudo pip3 install CrossMap
 else
   conda install -c bioconda samtools
   conda install -c bioconda bowtie2
+  conda install -c bioconda trim-galore
+  conda install -c bioconda fastqc
   conda install -c conda-forge unzip
   conda install -c bioconda crossmap
   #installazioni conda - inserire nella guida da dove si installa anaconda e di installare un ambiente python3
 fi
 
 cd $PATH_PROGRAM
+curl -fsSL https://github.com/FelixKrueger/TrimGalore/archive/0.6.6.tar.gz -o trim_galore.tar.gz
+tar xvzf trim_galore.tar.gz
 wget https://github.com/broadinstitute/picard/releases/download/2.26.2/picard.jar
 wget https://github.com/broadinstitute/gatk/releases/download/4.1.0.0/gatk-4.1.0.0.zip
 unzip gatk-4.1.0.0.zip
@@ -47,11 +54,15 @@ unzip master.zip
 mv varscan-master/VarScan.v2.4.3.jar ./
 cd
 cd $PATH_INDEX
+wget https://genome-idx.s3.amazonaws.com/bt/GRCh38_noalt_as.zip
+unzip GRCh38_noalt_as.zip
+wget https://genome-idx.s3.amazonaws.com/bt/hg19.zip
+unzip hg19.zip
 wget https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz
 gunzip hg38.fa.gz
 samtools faidx hg38.fa
-wget https://storage.googleapis.com/gcp-public-data--broad-references/hg19/v0/Homo_sapiens_assembly19.fasta
-mv Homo_sapiens_assembly19.fasta hg19.fa
+wget http://hgdownload.cse.ucsc.edu/goldenpath/hg19/bigZips/hg19.fa.gz
+gunzip hg19.fa.gz
 samtools faidx hg19.fa
 $PATH_JAVA -jar $PATH_PROGRAM/picard.jar CreateSequenceDictionary R=hg19.fa O=hg19.dict
 $PATH_JAVA -jar $PATH_PROGRAM/picard.jar CreateSequenceDictionary R=hg38.fa O=hg38.dict
